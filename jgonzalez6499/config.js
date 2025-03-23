dd94d4f78bfe6e0d0ee262103e3bc16eb775aff9
Jesus Gonzalez <chuso1982@gmail.com>
2025-03-23 20:40:19 +0100

    modificamos el gitignore

--- jgonzalez6499/config.js
+++
@@ -1,6 +0,0 @@
-// config.js
-const API_KEY_OMDBAPI = '1744ab12';
-const API_KEY_NASA = 'TU_API_KEY';
-const API_KEY_UNSPLASH = '6aJkvspqH8iYyVHBwvvA1oWduwuVxwUaA5m0bIrv1qQ';
-
-export { API_KEY_OMDBAPI, API_KEY_NASA, API_KEY_UNSPLASH };

aedf27114ac13f2481dff6b0c31b91630eda6c8f
Jesus Gonzalez <chuso1982@gmail.com>
2025-03-23 20:36:32 +0100

    .gitignore

--- configEdu.js
+++
@@ -1,6 +0,0 @@
-// config.js
-const API_KEY_OMDBAPI = '1744ab12';
-const API_KEY_NASA = 'TU_API_KEY';
-const API_KEY_UNSPLASH = 'TU_API_KEY';
-
-export { API_KEY_OMDBAPI, API_KEY_NASA, API_KEY_UNSPLASH };
